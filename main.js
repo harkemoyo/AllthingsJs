6468c87001cf209182c99207232d53e759abc31f
harkemoyo <markndeche91@gmail.com>
2023-06-26 12:58:01 +0300

    added html,css and js

--- main.js
+++ main.js
@@ -81,3 +81,24 @@ console.log(diff.parentElement)
 console.log(diff.parentNode)
 console.log(diff.children)
 console.log(diff.childNodes)
+
+
+// CloneNode.j
+
+const init = function(){
+    let t1, t2, div1, temp, div2, cln
+    t1 = document.getElementById('target1');
+    t2 = document.getElementById('target2');
+    
+    div1 = document.querySelector('.advertisement');
+    //for(let i=0; i<5; i++){
+        t1.appendChild(div1.cloneNode(true) );
+    //}
+    
+    temp = document.querySelector('[type="text/html"]');
+    cln = temp.cloneNode(true);
+    div2 = cln.textContent;
+    t2.innerHTML = div2;
+}
+
+document.addEventListener('DOMContentLoaded', init)
